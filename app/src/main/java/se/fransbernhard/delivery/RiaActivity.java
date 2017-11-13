69e6c9a630de3ef568d5f623c78b5ddcdd173f31
Ria <rialia@live.se>
2017-11-13 11:41:29 +0100

    Ria's activity

---
+++ app/src/main/java/se/fransbernhard/delivery/RiaActivity.java
@@ -0,0 +1,13 @@
+package se.fransbernhard.delivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ria);
+    }
+}
