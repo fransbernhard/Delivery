ab5ae137ce578f4bac74c1ad114237b101c64af6
Jean Melki <jeanmelki1@gmail.com>
2017-11-20 13:15:17 +0100

    StartActivity modifications

--- app/src/main/java/se/fransbernhard/delivery/SplashActivity.java
+++ app/src/main/java/se/fransbernhard/delivery/SplashActivity.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class StartActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private final int splashLength = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_splash);
         final Intent intent = new Intent(this, ListActivity.class);
 
         new Handler().postDelayed(new Runnable() {
