20a17149681cd92750ea9ebb5f0655a1de41e30d
Mimi Lundberg <mimilundberg@Mimis-MacBook-Pro-2.local>
2017-12-01 14:27:05 +0100

    Create onMapClick method to click from listView

--- app/src/main/java/se/fransbernhard/delivery/DetailActivity.java
+++ app/src/main/java/se/fransbernhard/delivery/DetailActivity.java
@@ -167,16 +167,4 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         mMap.animateCamera(CameraUpdateFactory.zoomIn());
         mMap.animateCamera( CameraUpdateFactory.zoomTo(15), 2000, null);
     }
-
-    public void mapClick(View v){
-        // Create a Uri from an intent string. Use the result to create an Intent.
-        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=" + latlong);
-        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        // Make the Intent explicit by setting the Google Maps package
-        mapIntent.setPackage("com.google.android.apps.maps");
-
-        // Attempt to start an activity that can handle the Intent
-        startActivity(mapIntent);
-    }
 }
--- app/src/main/java/se/fransbernhard/delivery/ListActivity.java
+++ app/src/main/java/se/fransbernhard/delivery/ListActivity.java
@@ -141,6 +141,18 @@ public class ListActivity extends AppCompatActivity {
         setAdapter();
     }
 
+    //    public void mapClick(View v){
+//        // Create a Uri from an intent string. Use the result to create an Intent.
+//        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=" + latlong);
+//        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//        // Make the Intent explicit by setting the Google Maps package
+//        mapIntent.setPackage("com.google.android.apps.maps");
+//
+//        // Attempt to start an activity that can handle the Intent
+//        startActivity(mapIntent);
+//    }
+
 
 }
 
